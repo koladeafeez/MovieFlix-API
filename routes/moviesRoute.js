55fc56f2f998d9da25d8697e4161db8b01ec95d0
kolade Afeez <daisedalok@gmail.com>
2019-10-21 13:28:26 +0100

    heroku configured

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -7,7 +7,7 @@ const moviesRoute = express.Router();
 
 moviesRoute.route('/')
 .get((req, res) => {
-    const url = process.env.MONGODB_URL || 'mongodb://localhost:27017/movieApp'
+    const url = process.env.MONGODB_URL || 'mongodb://localhost:27017'
     const dbName = 'moviesApp';
     (async function mongo(){
         let client;
