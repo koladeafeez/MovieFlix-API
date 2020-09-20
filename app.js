9df4684815be9dde7abdc71db5eef2d865501d34
kolade Afeez <daisedalok@gmail.com>
2020-09-20 12:21:29 +0100

    build

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const debug = require('debug')('app')
-const ejs = require('ejs');
-const { MongoClient, ObjectID } = require('mongodb')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const debug = require("debug")("app");
+const ejs = require("ejs");
+const { MongoClient, ObjectID } = require("mongodb");
 
- const adminRoute = require('./routes/adminRoute');
-const moviesRoute = require('./routes/moviesRoute');
+const adminRoute = require("./routes/adminRoute");
+const moviesRoute = require("./routes/moviesRoute");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var originsWhitelist = [
-    'http://localhost:4200',      //this is my front-end url for development
-     'https://nostalgic-jepsen-8d6aeb.netlify.com'
-  ];
-  var corsOptions = {
-    origin: function(origin, callback){
-          var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-          callback(null, isWhitelisted);
-    },
-    credentials:true
-  }
-  //here is the magic
-  app.use(cors(corsOptions));
+  "http://localhost:4200", //this is my front-end url for development
+  "https://happy-aryabhata-5c69ef.netlify.app",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+    callback(null, isWhitelisted);
+  },
+  credentials: true,
+};
+//here is the magic
+app.use(cors(corsOptions));
 
+app.use("/admin/movies", adminRoute);
+app.use("/api/movies", moviesRoute);
 
- app.use('/admin/movies', adminRoute);
-app.use('/api/movies', moviesRoute );
+app.set("view engine", ejs);
 
-app.set('view engine', ejs)
+app.get("/", (req, res) => {
+  res.send("route for movies");
+});
 
-app.get('/',(req, res)=>{
-  res.send('route for movies')
-})
-
-app.listen(port,() => {
-    debug("server running at port"+ port)
-})
+app.listen(port, () => {
+  debug("server running at port" + port);
+});
